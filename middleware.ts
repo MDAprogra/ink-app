1683ebff4579d9df884d0a70fe11deddac1f55cd
MatthiasInterfas <matthias@interfas.fr>
2025-12-04 10:41:02 +0100

    Connexion requise pour creer un article

--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,5 @@ export const config = {
   // Ici on liste toutes les routes à protéger.
   // Le pattern matcher permet de dire : "Tout ce qui commence par /catalogue ou /scan"
 //   matcher: ["/catalogue/:path*", "/scan/:path*", "/"],
-    matcher: ["/mouvements/:path*", "/scan/:path*"],
+    matcher: ["/mouvements/:path*", "/scan/:path*","/catalogue/nouveau"],
 };
