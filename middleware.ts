8000b3ae7e96e0963d09ed3d5f5d7b8459fa192e
MatthiasInterfas <matthias@interfas.fr>
2025-12-04 10:39:00 +0100

    Modif middleware

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,6 @@ export { default } from "next-auth/middleware";
 export const config = {
   // Ici on liste toutes les routes à protéger.
   // Le pattern matcher permet de dire : "Tout ce qui commence par /catalogue ou /scan"
-  matcher: ["/catalogue/:path*", "/scan/:path*", "/"],
+//   matcher: ["/catalogue/:path*", "/scan/:path*", "/"],
+    matcher: ["/mouvements/:path*", "/scan/:path*"],
 };
